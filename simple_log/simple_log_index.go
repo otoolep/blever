4544341ffe70310ee3ba8d2b0106a05291ffc43c
Philip O'Toole <philip.otoole@yahoo.com>
2015-03-06 21:13:08 -0800

    More stats and profiling of simple indexing

--- simple_log/simple_log_index.go
+++ simple_log/simple_log_index.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"log"
 	"os"
+	"runtime"
+	"runtime/pprof"
 	"strconv"
 	"time"
 
@@ -20,6 +22,17 @@ var memprofile = flag.String("memprofile", "", "write mem profile to file")
 func main() {
 	flag.Parse()
 
+	log.Printf("GOMAXPROCS: %d", runtime.GOMAXPROCS(-1))
+
+	// To profile, execute 'go tool pprof simple_log_index <pprof file>'
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+	}
+
 	// Attempt to open the file.
 	log.Printf("Opening log file %s", *logsPath)
 	logs, err := os.Open(*logsPath)
@@ -30,8 +43,11 @@ func main() {
 	// Read the lines into memory.
 	lines := make([]string, 0, 100000)
 	scanner := bufio.NewScanner(logs)
+	totalLen := 0
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		l := scanner.Text()
+		totalLen = totalLen + len(l)
+		lines = append(lines, l)
 	}
 
 	// Index them!
@@ -68,10 +84,13 @@ func main() {
 		}
 	}
 
+	pprof.StopCPUProfile()
+
 	indexDuration := time.Since(startTime)
 	indexDurationSeconds := float64(indexDuration) / float64(time.Second)
 	timePerDoc := float64(indexDuration) / float64(totalIndexed)
 
 	log.Print("indexing complete.")
-	log.Printf("Indexed %d documents, in %.2fs (average %.2fms/doc). %f/sec", totalIndexed, indexDurationSeconds, timePerDoc/float64(time.Millisecond), float64(totalIndexed)/indexDurationSeconds)
+	log.Printf("average line length: %d", totalLen/len(lines))
+	log.Printf("indexed %d documents, in %.2fs (average %.2fms/doc). %f/sec", totalIndexed, indexDurationSeconds, timePerDoc/float64(time.Millisecond), float64(totalIndexed)/indexDurationSeconds)
 }
